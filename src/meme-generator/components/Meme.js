a161d0adf5f3b11b9d4bd881c0bdd8e6bd617d29
singharsh0 <harshsingh22267@gmail.com>
2024-04-16 21:33:25 +0530

    useState implementation

--- src/meme-generator/components/Meme.js
+++ src/meme-generator/components/Meme.js
@@ -1,9 +1,45 @@
+import data from '../data';
+import React from "react";
+
 export default function Meme() {
+    /**
+     * Challenge: Update our state to save the meme-related
+     * data as an object called `meme`. It should have the
+     * following 3 properties:
+     * topText, bottomText, randomImage.
+     * 
+     * The 2 text states can default to empty strings for now,
+     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * 
+     * Next, create a new state variable called `allMemeImages`
+     * which will default to `memesData`, which we imported above
+     * 
+     * Lastly, update the `getMemeImage` function and the markup 
+     * to reflect our newly reformed state object and array in the
+     * correct way.
+     */
+    const meme = { topText: '', bottomText: '', randomImage: 'http://i.imgflip.com/1bij.jpg' }
+    const [memeImage, setMemeImage] = React.useState(meme);
+    const [allMemeImages, setAllMeme] = React.useState(data);
+
+    function generateMeme() {
+        const memeArray = allMemeImages.data.memes;
+        const randomNum = Math.floor(Math.random() * memeArray.length);
+        const { url } = memeArray[randomNum];
+        setMemeImage(prev => {
+            return {
+                ...prev,
+                randomImage: url
+            }
+        })
+        console.log(url)
+    }
     return (
-        <form className="form">
-            <input type="text" className="form--input" aria-aria-labelledby="Top Text" placeholder="Shut up" />
-            <input type="text" className="form--input" aria-label="Bottom Text" placeholder="Take My Money" />
-            <button className="form--button">Generate a new meme image 🖼</button>
-        </form>
+        <div className="form">
+            <input type="text" className="form--input" placeholder="Shut up" />
+            <input type="text" className="form--input" placeholder="Take My Money" />
+            <button className="form--button" onClick={generateMeme}>Generate a new meme image 🖼</button>
+            {memeImage.randomImage ? <img className="form--image" src={memeImage.randomImage} /> : ''}
+        </div>
     )
 }
